1689af873cf085e912226ec3852deffcbecca19f
Oleg Polyakov <olegpolyakov@outlook.com>
2019-10-23 16:59:02 +0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,5 @@
-const path = require('path');
 const gulp = require('gulp');
-const pug = require('gulp-pug');
 const sass = require('gulp-sass');
-
-// function buildPug() {
-//     return gulp.src('./src/views/index.pug')
-//         .pipe(pug())
-//         .pipe(gulp.dest('./public'));
-// }
 
 function buildSass() {
     return gulp.src('./src/styles/index.scss')
@@ -18,13 +10,6 @@ function buildSass() {
         .pipe(gulp.dest('./public/css/'));
 }
 
-function copyJs() {
-    return gulp.src('../node_modules/material-components-web/dist/material-components-web.min.js')
-        .pipe(gulp.dest('./build/'));
-}
-
 gulp.watch('./src/**/*.scss', buildSass);
-//gulp.watch('./src/**/*.pug', buildPug);
 
-//module.exports.default = gulp.parallel(buildPug, buildSass);
 module.exports.default = buildSass;
